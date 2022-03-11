5806f080f1f016e05a9fb8c246ead2044ea45193
Monday_David_Sarka <david_md@hamari.agency>
2022-03-11 14:00:50 +0100

    Added light and dark theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import useLocalStorage from 'use-local-storage';
+import Square from './components/square/Square';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+  }
+
   return (
-    <div className="App">
-      <h2>Hello World!</h2>
+    <div className="App" data-theme={theme}>
+      <span>LOGO</span>
+      <h2>Thanks for your Time David</h2>
+      <button onClick={switchTheme}>
+        Switch to {theme === 'light' ? 'dark' : 'light'} theme
+      </button>
+      <Square />
     </div>
   );
 }
---
+++ src/components/square/Square.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Square.css';
+
+const Square = () => {
+    return (
+        <div className="square">
+            <span>Hey Guyz!!, I'm a Circle!!!!!</span>
+        </div>
+    )
+}
+
+export default Square;
